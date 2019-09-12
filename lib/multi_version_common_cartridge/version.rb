26500a6bd2d12a4960334447706c0d84733ab23e
Adam Alboyadjian <adam@cassia.tech>
2019-09-12 15:51:39 -0300

    Bump version to 1.0.0.

--- lib/multi_version_common_cartridge/version.rb
+++ lib/multi_version_common_cartridge/version.rb
@@ -15,5 +15,5 @@
 # along with multi_version_common_cartridge.  If not, see <http://www.gnu.org/licenses/>.
 
 module MultiVersionCommonCartridge
-  VERSION = '0.0.1'.freeze
+  VERSION = '1.0.0'.freeze
 end
