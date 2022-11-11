cebd17d3dba0627237b5538014315294c163a920
Anita Patel <apatel@vistahigherlearning.com>
2022-11-11 14:33:11 +0000

    Bump version to 1.0.2

--- lib/multi_version_common_cartridge/version.rb
+++ lib/multi_version_common_cartridge/version.rb
@@ -15,5 +15,5 @@
 # along with multi_version_common_cartridge.  If not, see <http://www.gnu.org/licenses/>.
 
 module MultiVersionCommonCartridge
-  VERSION = '1.0.1'.freeze
+  VERSION = '1.0.2'.freeze
 end
